647f8e4abe212ee5ce207cee3dc86812977c6947
Elsani <elsan.memeti@hotmail.com>
2025-12-06 23:07:12 +0100

    Destructions

---
+++ destructuring/destructuring.js
@@ -0,0 +1,3 @@
+var _a = [][0], x = _a === void 0 ? 5 : _a;
+var _b = {}.prop, y = _b === void 0 ? 6 : _b;
+console.log(y);
---
+++ destructuring/destructuring.ts
@@ -0,0 +1,3 @@
+const [x = 5] = []
+const {prop:y = 6} = {};
+console.log(y);
